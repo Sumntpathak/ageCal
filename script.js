ba087cfcc78546cd3978581a8a4a9d2dff508f73
sumntpathak <88020516+Sumntpathak@users.noreply.github.com>
2022-03-17 23:39:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,119 @@
-const months = [31,28,31,30,31,30,31,31,30,31,30,31];
-
-function ageCalculate(){
-    let today = new Date();
-    let inputDate = new Date(document.getElementById("date-input").value);
-    let birthMonth,birthDate,birthYear;
-    let birthDetails = {
-        date:inputDate.getDate(),
-        month:inputDate.getMonth()+1,
-        year:inputDate.getFullYear()
-    }
-    let currentYear = today.getFullYear();
-    let currentMonth = today.getMonth()+1;
-    let currentDate = today.getDate();
-
-    leapChecker(currentYear);
-
-    if(
-        birthDetails.year > currentYear ||
-        ( birthDetails.month > currentMonth && birthDetails.year == currentYear) || 
-        (birthDetails.date > currentDate && birthDetails.month == currentMonth && birthDetails.year == currentYear)
-    ){
-        alert("Not Born Yet");
-        displayResult("-","-","-");
-        return;
-    }
-
-    birthYear = currentYear - birthDetails.year;
-
-    if(currentMonth >= birthDetails.month){
-        birthMonth = currentMonth - birthDetails.month;
-    }
-    else{
-        birthYear--;
-        birthMonth = 12 + currentMonth - birthDetails.month;
-    }
-
-    if(currentDate >= birthDetails.date){
-        birthDate = currentDate - birthDetails.date;
-    }
-    else{
-        birthMonth--;
-        let days = months[currentMonth - 2];
-        birthDate = days + currentDate - birthDetails.date;
-        if(birthMonth < 0){
-            birthMonth = 11;
-            birthYear--;
-        }
-    }
-    displayResult(birthDate,birthMonth,birthYear);
+//make years and days array
+function generateArray(start,end){         
+   let arr = [];
+   for(start; start <= end; start++){
+     arr.push(start);
+   }
+   return arr;
 }
 
-function displayResult(bDate,bMonth,bYear){
-    document.getElementById("years").textContent = bYear;
-    document.getElementById("months").textContent = bMonth;
-    document.getElementById("days").textContent = bDate;
+const months =  ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']; 
+//1-31
+const days = generateArray(1,31);
+//1900-today
+const years = generateArray(1900,(new Date).getFullYear());
+
+//calculate age
+function calculateAge(birthday){
+  //milliseconds in a year 1000*24*60*60*365.24 = 31556736000; 
+  let today = new Date(),
+      //birthay has 'Dec 25 1998'
+      dob = new Date(birthday),
+      //difference in milliseconds
+      diff = today.getTime() - dob.getTime(),
+      //convert milliseconds into years
+      years = Math.floor(diff / 31556736000),
+      //1 day has 86400000 milliseconds
+      days_diff= Math.floor((diff % 31556736000) / 86400000),
+      //1 month has 30.4167 days
+      months = Math.floor(days_diff / 30.4167),
+      days = Math.floor(days_diff % 30.4167);
+     
+     console.log(`${years} years ${months} months ${days} days`);
+     return `${years} years ${months} months ${days} days`;
 }
 
-function leapChecker(year){
-    if(year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)){
-        months[1] = 29;
-    }
-    else{
-        months[1] = 28;
-    }
+
+class AgeCalculator extends React.Component{
+  constructor(props){ 
+    super(props);
+    this.state = {
+      day : 25,
+      month : 'Dec',
+      year :  1998,
+      age: '20 years 11 months 28 days'
+    };
+    
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDayChange = this.handleDayChange.bind(this);
+    this.handleMonthChange = this.handleMonthChange.bind(this);
+    this.handleYearChange = this.handleYearChange.bind(this);
+  }
+
+  handleDayChange(e){
+    this.setState({
+      day: e.target.value 
+    });
+  }
+
+  handleMonthChange(e){
+    this.setState({
+      month: e.target.value 
+    });
+  }
+
+  handleYearChange(e){
+    this.setState({
+      year: e.target.value 
+    });
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+
+    const day = this.state.day,
+        month = this.state.month,
+        year = this.state.year;
+    
+    let age =  calculateAge(`${month} ${day} ${year}`);
+
+    this.setState({
+      age: age
+    });
+
+  }
+
+  render(){
+
+    return <div>
+      <h1>Age Calculator</h1>
+      <form onSubmit = {this.handleSubmit}>
+        <div className = "container">
+          <Input arr = {days} handleChange = {this.handleDayChange} val = {this.state.day} />
+          <Input arr = {months} handleChange = {this.handleMonthChange} val = {this.state.month} />
+          <Input arr = {years} handleChange = {this.handleYearChange} val = {this.state.year} />
+        </div>
+        <button type="submit">Calculate</button>
+      </form>
+      <article>
+        <h2>Your age is</h2>
+         <span>{this.state.age}</span>
+      </article>
+      <footer>Made with <a href="https://reactjs.org/" target="_blank" >ReactJS</a>
+      </footer>
+    </div>;
+  }
+
 }
+
+function Input(props){
+  let options = props.arr.map((item)=> <option value={item} key={item}>{item}</option>);
+                              
+   return  <select onChange = {props.handleChange} value={props.val}>
+               {options}
+            </select>;
+   }
+
+
+
+ReactDOM.render(<AgeCalculator />,document.getElementById('root'));
